adf19b11a33318dee148313b42f98a899eae8b00
angularadmin <Zen_json@163.com>
2017-07-29 19:08:27 +0800

    spring boot learn

---
+++ Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
